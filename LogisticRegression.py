236c4b89ea8a1d391af3b4bce43f36277eed2aca
aibironke <116117927+aibironke@users.noreply.github.com>
2023-03-01 13:27:00 -0600

    Add files via upload

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -5,9 +5,6 @@ Created on Wed Mar  1 01:39:21 2023
 @author: postgres
 """
 
-
-
-
 # Load Libraries
 import os
 import pandas as pd
@@ -34,45 +31,23 @@ os.chdir(path)
 a = pd.read_csv('Wells_Params.csv')
 a.head(5) # explore first 5 rows
 
-# Arranging variables to perform regression
-#Extracting columns to be used as X variables
-cols = [7,8,9,17,18]
-X = a[a.columns[cols]]
-Y = a['Avg_Nit'] # strength is the Y variable
-
-# Scale the independent variables using StandardScaler()
-scaler = StandardScaler()
-X_scaled = scaler.fit_transform(X)
-
-# Replace the original independent variables with the scaled variables in the DataFrame
-X.iloc[:, :5] = X_scaled
+b = a.copy()
 
 # Define the threshold for reclassifying y
 threshold = 3
 
 # Reclassify y based on the threshold
-y_reclassified = np.where(Y >= threshold, 1, 0)
+b['Avg_Nit'] = np.where(b['Avg_Nit'] >= threshold, 1, 0)
 
-# Fit a logistic regression model
-lr_model = LogisticRegression()
-lr_model.fit(X_scaled, y_reclassified)
-
-# Make predictions on the original y values
-y_pred = lr_model.predict_proba(X_scaled)[:, 1]
-y_pred_reclassified = np.where(y_pred >= threshold, 1, 0)
-
-# Evaluate the model
-acc = accuracy_score(y_reclassified, y_pred_reclassified)
-precision = precision_score(y_reclassified, y_pred_reclassified)
-recall = recall_score(y_reclassified, y_pred_reclassified)
-f1 = f1_score(y_reclassified, y_pred_reclassified)
-
-print(f"Accuracy: {acc}")
-print(f"Precision: {precision}")
-print(f"Recall: {recall}")
-print(f"F1 score: {f1}")
+# Arranging variables to perform regression
+#Extracting columns to be used as X variables
+cols = [7,8,9,17,18]
+X = a[a.columns[cols]]
+Y = b['Avg_Nit'] # strength is the Y variable
 
 
+X_scaled = (X - X.min())/(X.max()-X.min())
+y_reclassified = Y
 
 # Split into training and testing data
 X_train,X_test,y_train,y_test=train_test_split(X_scaled,y_reclassified,test_size=0.30,random_state=10)
@@ -80,33 +55,67 @@ X_train,X_test,y_train,y_test=train_test_split(X_scaled,y_reclassified,test_size
 # instantiate the model object (using the default parameters)
 logreg = LogisticRegression(C=10**9) # Large C means no regularization
 
+#Training the data
 # fit the model with data
 logreg.fit(X_train,y_train)
 
 
 # Make Predictions
-y_pred=logreg.predict(X_test) # Make Predictions
-yprob = logreg.predict_proba(X_test) #test output probabilities
+y_pred=logreg.predict(X_train) # Make Predictions
+yprob = logreg.predict_proba(X_train) #test output probabilities
 zz = pd.DataFrame(yprob)
-zz.to_csv('nit_a.csv')
+#zz.to_csv('nit_a.csv')
 
 # Get the parameters
 logreg.get_params()
 logreg.coef_
 logreg.intercept_
 
-# Write the data to a file
-keys = list(X.columns)
-keys.append('Intercept')
+# Create a confusion Matrix and plot it
+cnf_matrix = metrics.confusion_matrix(y_train, y_pred)
+cnf_matrix # y_test is going be rows (obs), y_pred (predicted) are cols
+disp = metrics.ConfusionMatrixDisplay(confusion_matrix=cnf_matrix)
+disp.plot()
+plt.show()
 
-vals = logreg.coef_.tolist()
-vals = list(chain.from_iterable(vals))
-intcept = float(logreg.intercept_)
-vals.append(intcept)
+# Evaluate the model
+acc = accuracy_score(y_train, y_pred)
+precision = precision_score(y_train, y_pred)
+recall = recall_score(y_train, y_pred)
+f1 = f1_score(y_train, y_pred)
 
-par_dict = dict(zip(keys,vals))
-with open('pars.txt','w') as data: 
-      data.write(str(par_dict))
+print(f"Accuracy: {acc}")
+print(f"Precision: {precision}")
+print(f"Recall: {recall}")
+print(f"F1 score: {f1}")
+
+
+# ROC Curve
+y_pred_proba = logreg.predict_proba(X_train)[::,1]
+fpr, tpr, _ = metrics.roc_curve(y_train,  y_pred_proba)
+auc = metrics.roc_auc_score(y_train, y_pred_proba)
+plt.plot(fpr,tpr,label="data 1, auc="+str(round(auc,4)))
+plt.legend(loc=4)
+plt.xlabel('1-Specificity')
+plt.ylabel('Sensitivity')
+plt.grid()
+plt.show()
+
+#Testing the data
+# fit the model with data
+logreg.fit(X_test,y_test)
+
+
+# Make Predictions
+y_pred=logreg.predict(X_test) # Make Predictions
+yprob = logreg.predict_proba(X_test) #test output probabilities
+zz = pd.DataFrame(yprob)
+#zz.to_csv('nit_a.csv')
+
+# Get the parameters
+logreg.get_params()
+logreg.coef_
+logreg.intercept_
 
 # Create a confusion Matrix and plot it
 cnf_matrix = metrics.confusion_matrix(y_test, y_pred)
@@ -116,10 +125,16 @@ disp.plot()
 plt.show()
 
 # Evaluate the model
-acc = accuracy_score(y_reclassified, y_pred_reclassified)
-precision = precision_score(y_reclassified, y_pred_reclassified)
-recall = recall_score(y_reclassified, y_pred_reclassified)
-f1 = f1_score(y_reclassified, y_pred_reclassified)
+acc = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+
+print(f"Accuracy: {acc}")
+print(f"Precision: {precision}")
+print(f"Recall: {recall}")
+print(f"F1 score: {f1}")
+
 
 # ROC Curve
 y_pred_proba = logreg.predict_proba(X_test)[::,1]
